cd8e33e0316f7d81f9bb1b16bd0607522bf2a8bd
Kavya Bammidi <kavyabammidi28@gmail.com>
2025-12-07 18:54:36 +0530

    Add files via upload

---
+++ javascript/variables/app.js
@@ -0,0 +1,16 @@
+
+
+//var data = "javasript"
+//var data= "hello ";
+
+//console.log('data')
+//console.log(data)
+
+let dat;
+data = "hello python";
+console.log(data)
+
+const data = "hello";
+data = "js"
+console.log(data)
+
