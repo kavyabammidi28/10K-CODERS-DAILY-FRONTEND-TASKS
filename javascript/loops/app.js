ff9e253f2c9ebe858263a2e86954ef7fe8366b16
Kavya Bammidi <kavyabammidi28@gmail.com>
2025-12-07 18:45:06 +0530

    Add files via upload

---
+++ javascript/loops/app.js
@@ -0,0 +1,126 @@
+//for loop
+
+// for(let i=0;i<=10;i++){
+//     console.log(i)
+// }
+
+// for(let i=0;i>=10;i--){
+//     console.log(i)
+// }
+
+// console.log(10);
+// console.log(9);
+
+
+//for loop
+// let data = [1,2,3,4,5]
+// for(let i=0;i<=4;i++){
+//     console.log(data[i])
+// }
+
+// let data = [
+//     {name: "kaavs",loc: "hyd"},
+//     {name: "somss",loc: "hyd"},
+//     {name: "roops",loc: "hyd"}
+// ]
+
+// for(let i=0;i<=2;i++){
+//     console.log(data[i])
+// }
+
+// for(let i=0;i<=2;i++){
+//     console.log(data[i].name)
+// }
+
+// for(let i=0;i<=2;i++){
+//     console.log(data[i].loc)
+// }
+
+// let data = [
+//     {name: "kaavs",loc: "hyd"},
+//     {name: "somss",loc: "hyd"},
+//     {name: "roops",loc: "hyd"}
+// ]
+
+// // data.forEach((items)=>{
+// //     console.log(items)
+// // })
+
+// data.forEach((value,indexe,array)=>{
+//     console.log(value)
+// })
+// data.forEach((value,indexe,array)=>{
+//     console.log(ind)
+// })
+// data.forEach((value,indexe,array)=>{
+//     console.log(value)
+// })
+
+
+// let hero = {
+//     name: "ram",
+//     age: 29,
+//     ccurrentMovie:"AKT"
+// }
+
+// for(let i in hero){
+//     console.log(i)
+// }
+
+// let data = [1,2,3,4,5]
+// for(let i of data){
+//     console.log(i)
+// }
+
+// let i = 1;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 1;
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=5);
+// while(i<=5){
+//     console.log(i
+//         i++;
+//     )
+// }
+
+// for (let i=1;i<5;i++){
+//     if(i==3){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 1;
+// while(i<=5){
+//     i++;
+//     if(i==3){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+// let i = 1;
+// while(i<=5){
+//     i++;
+//     if(i==3){
+//         continue;
+//     }
+//     console.log(i);
+// }
+// for (let i=1;i<5;i++){
+//     if(i==3){
+//         continue;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+
