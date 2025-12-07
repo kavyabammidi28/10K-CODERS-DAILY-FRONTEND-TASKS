3ec9e61a97b5e3c8ab35f269d95e5cd2e80c85a7
Kavya Bammidi <kavyabammidi28@gmail.com>
2025-12-07 18:40:08 +0530

    Add files via upload

---
+++ javascript/arrays/app.js
@@ -0,0 +1,23 @@
+let data = ["apple","mango","guava","kiwi","orange"]
+console.log(data.slice(0,3))
+
+
+// console.log(data.length)
+// console.log(data.indexOf("kiwi"))
+// console.log(data.indexOf("pineapple"))
+// console.log(data.push("pomogranatee"))
+// console.log(data)
+// console.log(data.pop())
+// console.log(data)
+// console.log(data.unshift("pomogranate"))
+// console.log(data)
+// console.log(data.length)
+// console.log(data.includes("apple"))
+// console.log(data.reverse())
+
+// let fruit =  ["custardapple"]
+// console.log(data.concat(fruit))
+
+// let data =" i love my family"
+// console.log(data.join())
+// console.timeLog(data.split())
