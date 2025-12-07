ab06de7e564c6726cc3c84cd98557d8d22494649
Kavya Bammidi <kavyabammidi28@gmail.com>
2025-12-07 18:43:49 +0530

    Add files via upload

---
+++ javascript/conditional statements/app.js
@@ -0,0 +1,63 @@
+
+
+// let data = 523;
+// //  if (data < 600){
+//     if (data > 600){
+//         console.log("u r guess is crct")
+//     }
+//     console.log("u r guess is crct")
+
+
+// let data = 523;
+// if (data > 600){
+//     console.log("crct") 
+// }
+// else{
+//     console.log("incrct")
+// }
+
+// let num = prompt("enter a number: ")
+// console.log(num)
+
+// let age=23;
+// if (age<10){
+//     console.log("kid")
+// }
+// else if(age>20){
+//     console.log("young")
+// }
+// else if(age>=45){
+//     console.log("uncle")
+// }
+// else if(age>=70){
+//     console.log("grand father")
+// }
+// else{
+//     console.log("take rest")
+// }
+
+
+// let num=4;
+// switch(num){
+//     case 1:
+//         console.log("value is 1");
+//         break;
+//     case 2:
+//         console.log("value is 2");
+//         break;
+//     case 3:
+//         console.log("value is 3");
+//         break;
+//     case 4:
+//         console.log("value is 4");
+//         break;
+//     case 5:
+//         console.log("value is 5");
+//         break;
+//     default:
+//         console.log("value is nothing")
+// }
+
+let age = 10;
+let result = (age >= 18)? "hello":"hii"
+console.log(result)
