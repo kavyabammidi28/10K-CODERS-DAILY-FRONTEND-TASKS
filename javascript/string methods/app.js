48163796444ae3427bdd6198a6d086af1aedf7e9
Kavya Bammidi <kavyabammidi28@gmail.com>
2025-12-07 18:53:13 +0530

    Add files via upload

---
+++ javascript/string methods/app.js
@@ -0,0 +1,28 @@
+// let data=
+// hello
+// students
+// good 
+// afternoon 
+// ;
+// console.log(data)
+ 
+
+// let name = "basha";
+// let greet = "afternoon";
+// console.log(`hello ${name} good ${greet}`)
+
+// let x = 15;
+// let y = 20;
+// console.log(`th sum of ${x} and ${y} is ${x+y}`)
+
+
+let data = "hello this is javascript class "
+console.log(data.length)
+console.log(data.lastIndexOf('i'))
+console.log(data.indexOf('z'))
+console.log(data.endswith('s'))
+console.log(data.trim())
+console.log(data.replace("hello","hii"))
+console.log(data.slice(2,8))
+console.log(data.includes("na"))
+console.log(data.charAt('c'))
