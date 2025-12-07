c075b07ea0b61d7ebeb172db7189adfc9c66f878
Kavya Bammidi <kavyabammidi28@gmail.com>
2025-12-07 18:51:01 +0530

    Add files via upload

---
+++ javascript/Data types /Non primitive/app.js
@@ -0,0 +1,26 @@
+let hero = {
+    name: "prabhas",
+    prof:"actor",
+    movies:20,
+    currentMovie:"Rajasaab"
+}
+
+//console.log(hero)
+//console.log(hero['name'])
+
+//hero['maritalstatus'] = false
+//delete hero['movies']
+
+// hero['name'] = "prabhas"
+// console.log(hero);
+
+// let course = ["html",1,2,"css",false,3,4,"javascript",[1,2,3],{name:"basha"}]
+// delete course[3]
+// course[10] = 'java'
+// console.log(course)
+// course[3] = "bootstrap";
+
+
+// create 3 objects in array each object contains name and 
+// loc as keys i need second object loc value as output
+
